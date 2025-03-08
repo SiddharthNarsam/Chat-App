73dd462e04bf643feef8186264ff9518ee73bf14
SiddharthNarsam <sknarsam@gmail.com>
2025-03-08 17:59:50 +0530

    Connected mongodb and done with signup backend with jwt auth token

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,6 +1,62 @@
-export const signup=(req,res)=>{
-    res.send("Signup Route");
+import { generateToken } from "../libs/utils.js";
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
+
+
+export const signup= async(req,res)=>{
+    const {fullName,email,password} = req.body;
+    try{
+        //check email and name field is not null
+        if(!fullName || !email || !password ){
+            return res.status(400).json({message: "All fields are required"});
+        }
+
+        // check password
+        if(password.length < 6){
+            return res.status(400).json({message:"Password must be atleast 6 charecters."});
+        }
+
+        // finding if the user already exists    
+        const user = await User.findOne({email});
+        if (user) return res.status(400).json({message:"Email already exits !"});
+
+        // hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+
+        const newUser = new User ({
+            fullName : fullName,
+            email : email,
+            password : hashedPassword // password  
+        })
+
+        if(newUser){
+            //Generate JWT token here
+            generateToken(newUser._id,res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic: newUser.profilePic,
+            });
+        }
+        else{
+            res.status(400).json({message : "Invalid user data."});
+        }
+
+
+    } catch (error) {
+        console.log("Error in signup constroller:", error.message);
+        res.status(500).json({message:"Internal Server Error"});
+    }
+    
+    // res.send("Signup Route");
 };
+
+
 
 export const login = (req,res)=>{
     res.send("Login Route");
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,23 @@
 // const express = require("express");
 
-import express from "express";
+import express, { json } from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./libs/db.js";
+
 
 const app = express();
 
+dotenv.config();
+
+const PORT= process.env.PORT
+
+app.use(express.json())
+
+
 app.use("/api/auth",authRoutes)
 
-app.listen(5001,()=>{
-    console.log("Server is running on port 5001");
+app.listen(PORT,()=>{
+    console.log(`Server is running on port: ${PORT}`);
+    connectDB();
 })
